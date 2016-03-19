15659301492b9bd2b9ea3064ac65b91237a8fd02
OpenGelo <cucueinstein.mfa@gmail.com>
2016-03-19 09:20:07 +0700

    GUI Antenna Tracker
    GUI for GPS Based Antenna Tracker

---
+++ GCSTracker/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace GCSTracker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.MainMap = new GMap.NET.WindowsForms.GMapControl();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.serialPort2 = new System.IO.Ports.SerialPort(this.components);
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // MainMap
+            // 
+            this.MainMap.Bearing = 0F;
+            this.MainMap.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.MainMap.CanDragMap = true;
+            this.MainMap.EmptyTileColor = System.Drawing.Color.Navy;
+            this.MainMap.GrayScaleMode = false;
+            this.MainMap.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.MainMap.LevelsKeepInMemmory = 5;
+            this.MainMap.Location = new System.Drawing.Point(12, 12);
+            this.MainMap.MarkersEnabled = true;
+            this.MainMap.MaxZoom = 20;
+            this.MainMap.MinZoom = 2;
+            this.MainMap.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.MainMap.Name = "MainMap";
+            this.MainMap.NegativeMode = false;
+            this.MainMap.PolygonsEnabled = true;
+            this.MainMap.RetryLoadTile = 0;
+            this.MainMap.RoutesEnabled = true;
+            this.MainMap.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.MainMap.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.MainMap.ShowTileGridLines = false;
+            this.MainMap.Size = new System.Drawing.Size(290, 204);
+            this.MainMap.TabIndex = 0;
+            this.MainMap.Zoom = 17D;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(333, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(518, 32);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(333, 146);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(122, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(471, 146);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(122, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(333, 196);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(122, 20);
+            this.textBox4.TabIndex = 6;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(471, 196);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(122, 20);
+            this.textBox5.TabIndex = 7;
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(333, 75);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(251, 44);
+            this.richTextBox1.TabIndex = 8;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(333, 251);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(122, 20);
+            this.textBox1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(628, 320);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.MainMap);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private GMap.NET.WindowsForms.GMapControl MainMap;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.IO.Ports.SerialPort serialPort2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ GCSTracker/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GMap.NET;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+using GMap.NET.MapProviders;
+using GMap.NET.Projections;
+using System.IO.Ports;
+using System.Text.RegularExpressions;
+
+namespace GCSTracker
+{
+    public partial class Form1 : Form
+    {
+        double latitude = -6.977865167;
+        double longitude = 107.630188833;
+        PointLatLng current_pos;
+        PointLatLng home_pos;
+        public Form1() 
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Initialize map:
+            MainMap.MapProvider = GMap.NET.MapProviders.BingSatelliteMapProvider.Instance;
+            GMaps.Instance.Mode = AccessMode.ServerOnly;
+            MainMap.Position = new PointLatLng(-6.976865167, 107.630188833);
+            
+            //MainMap.Position = new PointLatLng(-25.971684, 32.589759);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            serialPort1.PortName = "COM3";
+            serialPort1.BaudRate = 57600;
+            serialPort1.StopBits = StopBits.One;
+            serialPort1.DataBits = 8;
+            //serialPort1.Handshake = Handshake.None;
+            serialPort1.Parity = Parity.None;
+            serialPort1.Open();
+            GMapOverlay homeOverlay = new GMapOverlay("markers");
+            GMarkerGoogle home = new GMarkerGoogle(new PointLatLng(latitude, longitude), GMarkerGoogleType.blue);
+            homeOverlay.Markers.Clear();
+            MainMap.Overlays.Add(homeOverlay);
+            homeOverlay.Markers.Add(home);
+            serialPort1.Write("1");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            serialPort2.PortName = "COM6";
+            serialPort2.BaudRate = 57600;
+            serialPort2.StopBits = StopBits.One;
+            serialPort2.DataBits = 8;
+            //serialPort1.Handshake = Handshake.None;
+            serialPort2.Parity = Parity.None;
+            serialPort2.Open();
+        }
+
+        string kata;
+        string[] words;
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            kata = serialPort1.ReadLine();
+            words = Regex.Split(kata, " ");
+            if (words[0] == "005" && words.Length == 12)
+            {
+                richTextBox1.BeginInvoke(new myDelegate(updatetextbox));
+            }
+        }
+
+        public delegate void myDelegate();
+
+        double lat,lng;
+        void updatetextbox()
+        {
+            lat = Convert.ToDouble(words[10]);
+            lng = Convert.ToDouble(words[11]);
+            
+            richTextBox1.Text = kata;
+
+            GMapOverlay markersOverlay = new GMapOverlay("markers");
+            GMarkerGoogle marker = new GMarkerGoogle(new PointLatLng(lat, lng), GMarkerGoogleType.green);
+            markersOverlay.Markers.Clear();
+            MainMap.Overlays.Add(markersOverlay);
+            markersOverlay.Markers.Add(marker);
+            MainMap.Invalidate(false);
+
+            textBox2.Text = words[10];
+            textBox3.Text = words[11];
+            textBox4.Text = Convert.ToString(BearingTo(lat, lng));
+            textBox5.Text = Convert.ToString(RhumbBearingTo(lat,lng));
+            
+            home_pos.Lat = latitude;
+            home_pos.Lng = longitude;
+            
+            current_pos.Lat = lat;
+            current_pos.Lng = lng;
+            double bear = MainMap.MapProvider.Projection.GetBearing(home_pos, current_pos);
+            textBox1.Text = Convert.ToString(bear);
+        }
+
+        public enum DistanceType : int
+        {
+            Miles = 0,
+            Kilometers = 1
+        }
+
+        public double DegreeToRadian(double angle) { return Math.PI * angle / 180.0; }
+
+        public const double EarthRadiusInMiles = 3956.0;
+        public const double EarthRadiusInKilometers = 6367.0;
+
+        public double RadianToDegree(double angle) { return 180.0 * angle / Math.PI; }
+
+        public double DistanceTo(double lat, double lng, DistanceType dType)
+        {
+            double R = (dType == DistanceType.Miles) ? EarthRadiusInMiles : EarthRadiusInKilometers;
+            double dLat = DegreeToRadian(lat) - DegreeToRadian(this.latitude);
+            double dLon = DegreeToRadian(lng) - DegreeToRadian(this.longitude);
+            double a = Math.Sin(dLat / 2) * Math.Sin(dLat / 2) + Math.Cos(DegreeToRadian(this.latitude)) * Math.Cos(DegreeToRadian(lat)) * Math.Sin(dLon / 2) * Math.Sin(dLon / 2);
+            double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+            double distance = c * R;
+
+            return Math.Round(distance, 2);
+        } // end DistanceTo
+
+        public double RhumbDistanceTo(double lat, double lng, DistanceType dType)
+        {
+            double R = (dType == DistanceType.Miles) ? EarthRadiusInMiles : EarthRadiusInKilometers;
+            double lat1 = DegreeToRadian(this.latitude);
+            double lat2 = DegreeToRadian(lat);
+            double dLat = DegreeToRadian(lat - this.latitude);
+            double dLon = DegreeToRadian(Math.Abs(lng - this.longitude));
+
+            double dPhi = Math.Log(Math.Tan(lat2 / 2 + Math.PI / 4) / Math.Tan(lat1 / 2 + Math.PI / 4));
+            double q = Math.Cos(lat1);
+            if (dPhi != 0) q = dLat / dPhi;  // E-W line gives dPhi=0
+            // if dLon over 180° take shorter rhumb across 180° meridian:
+            if (dLon > Math.PI) dLon = 2 * Math.PI - dLon;
+            double dist = Math.Sqrt(dLat * dLat + q * q * dLon * dLon) * R;
+
+            return dist;
+        } // end RhumbDistanceTo
+
+        public double RhumbBearingTo(double lat, double lng)
+        {
+            double lat1 = DegreeToRadian(this.latitude);
+            double lat2 = DegreeToRadian(lat);
+            double dLon = DegreeToRadian(lng - this.longitude);
+
+            double dPhi = Math.Log(Math.Tan(lat2 / 2 + Math.PI / 4) / Math.Tan(lat1 / 2 + Math.PI / 4));
+            if (Math.Abs(dLon) > Math.PI) dLon = (dLon > 0) ? -(2 * Math.PI - dLon) : (2 * Math.PI + dLon);
+            double brng = Math.Atan2(dLon, dPhi);
+
+            return (RadianToDegree(brng) + 360) % 360;
+        } // end RhumbBearingTo
+
+        public double BearingTo(double lat, double lng)
+        {
+            double lat1 = DegreeToRadian(this.latitude);
+            double lat2 = DegreeToRadian(lat);
+            double dLon = DegreeToRadian(lng) - DegreeToRadian(this.longitude);
+
+            double y = Math.Sin(dLon) * Math.Cos(lat2);
+            double x = Math.Cos(lat1) * Math.Sin(lat2) - Math.Sin(lat1) * Math.Cos(lat2) * Math.Cos(dLon);
+            double brng = Math.Atan2(y, x);
+
+            return (RadianToDegree(brng) + 360) % 360;
+        } // end BearingTo
+    }
+}
